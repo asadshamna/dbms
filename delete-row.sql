d1fc0c6aa99b130e82bfddef38877ad980a1c249
Asad <asadshamna02@gmail.com>
2023-12-18 22:05:22 +0530

    CRUD operation Part 2

---
+++ delete-row.sql
@@ -0,0 +1,3 @@
+-- DELETE FROM users WHERE id = 1;
+
+DELETE FROM users WHERE id >= 4;
---
+++ update-rows.sql
@@ -0,0 +1,7 @@
+UPDATE users 
+SET std = '3rd'
+WHERE id = 1;
+
+UPDATE users 
+SET std = '4th'
+WHERE id = 2;
